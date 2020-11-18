912c6e7ecfd4b3c74ad7fe3de93065392e757046
r4ph3rd <hav4na@protonmail.com>
2020-11-18 16:02:06 +0100

    push build to gh pages

---
+++ snake-quizz/dist/precache-manifest.2a133ca594413e7e30cd1052b3d60538.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af170e7d2dfec41ecf08",
+    "url": "/css/app.a86dd241.css"
+  },
+  {
+    "revision": "ab2e37f4ddc0e6789c16",
+    "url": "/css/chunk-8f9f2e78.abcfad95.css"
+  },
+  {
+    "revision": "e74a486151a0cbe1110e",
+    "url": "/css/chunk-e84271fa.a653f2c5.css"
+  },
+  {
+    "revision": "f796fcb22a9e0911da1920a83b86879c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af170e7d2dfec41ecf08",
+    "url": "/js/app.8be1b69f.js"
+  },
+  {
+    "revision": "ab2e37f4ddc0e6789c16",
+    "url": "/js/chunk-8f9f2e78.7974c385.js"
+  },
+  {
+    "revision": "e74a486151a0cbe1110e",
+    "url": "/js/chunk-e84271fa.d459d9ae.js"
+  },
+  {
+    "revision": "c727892ca6a18d49a884",
+    "url": "/js/chunk-vendors.90e079b0.js"
+  },
+  {
+    "revision": "3134d22a941aa417c19e0c5ff462a6b8",
+    "url": "/js/game.js"
+  },
+  {
+    "revision": "52fb87e418c0c5747becc19c20c22bee",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
